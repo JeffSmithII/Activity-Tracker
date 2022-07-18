72554680f63e79ccdb9bea0a475ac7c13cb3bf94
Jeffries Smith <Jeffsmithii@yahoo.com>
2022-07-18 14:01:16 -0400

    Adding features from Feature List

---
+++ script2.js
@@ -0,0 +1,14 @@
+let date1 = new Date();
+let date2 = new Date('12/25/2022');
+
+let btnGet = document.querySelector('button');
+let result = document.querySelector('h3');
+
+btnGet.addEventListener('click', () => {
+    let diff = date2.getTime() - date1.getTime();
+
+    let msInDay = 1000 * 3600 * 24;
+
+    result.innerText = diff/msInDay;
+
+});
