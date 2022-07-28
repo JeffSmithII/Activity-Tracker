96fc52715aae94980abeb6df8a3a635f633068b2
Jeffries Smith <Jeffsmithii@yahoo.com>
2022-07-27 20:41:22 -0400

    feature fixes tht are more inline with description

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
 let btnShow = document.querySelector('button');
-let output = document.querySelector('h1');
+let output = document.getElementById('dlity');
 
 btnShow.addEventListener('click', () => {
     let today = new Date();
+    let eoy = new Date('12/31/2022')
 
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-    let date = today.getDate();
+    let difference = eoy.getTime() - today.getTime();
 
-    let current_date = `${month}/${date}/${year}`;
-    output.innerText = current_date;
+    let msInDay = 1000 * 3600 * 24;
 
-
-    let hours = addZero(today.getHours());
-    let minutes = addZero(today.getMinutes());
-    let seconds = addZero(today.getSeconds());
-
-    let current_time = `${hours}:${minutes}:${seconds}`;
-    //output.innerText = current_time;
-
-});
-
-function addZero(num){
-    return num < 10 ? `0${num}`:num;
-}
+    output.innerText = difference/msInDay;
+})
